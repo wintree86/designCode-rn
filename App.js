4dadd696ff1cb2ba5773d3d37f4caafc22bae566
RyanCho <mith1004@gmail.com>
2019-05-01 12:30:37 +0900

    init commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
+
+const initialState = {
+  action: ""
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "OPEN_MENU":
+      return { action: "openMenu" };
+    case "CLOSE_MENU":
+      return { action: "closeMenu" };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const App = () => (
+  <Provider store={store}>
+    <HomeScreen />
+  </Provider>
+);
+
+export default App;
